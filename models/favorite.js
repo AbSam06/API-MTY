50c2833e79e05f4490e66787f9a984748d13eaae
Mohamed Hanou <66230414+moha-hn@users.noreply.github.com>
2025-04-30 14:58:43 -0400

    Update favorite.js

--- models/favorite.js
+++ models/favorite.js
@@ -4,10 +4,9 @@ const sequelize = require('../config/db');
 const Favorite = sequelize.define('Favorite', {
   user_id: { type: DataTypes.INTEGER, allowNull: false },
   news_id: { type: DataTypes.INTEGER, allowNull: false },
+  createdAt: { type: DataTypes.DATE, allowNull: false }, // Explicitly define createdAt
 }, {
-  timestamps: true,
-  createdAt: 'createdAt',
-  updatedAt: false,
+  timestamps: false, // Disable automatic timestamp management
   indexes: [
     {
       unique: true,
